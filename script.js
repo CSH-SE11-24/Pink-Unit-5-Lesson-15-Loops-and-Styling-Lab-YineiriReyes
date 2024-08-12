b5d2c6a14cd1b50f469f8e23ca3f0eb46063ca91
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:30:00 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Script running...");
+// Task 1: All off button
+// Set up the the DOM manipulation flow such that when you press the all off button, the background color of all the lightbulb p tags changes to black
+
+
+
+
+
+
+
+
+
+// Task 2: CSH Mode
+// Set up the DOM manipulation flow such that when your mouse is over the CSH mode button, the body's background color changes to green and the lightbulb p tags text change to "💻"
+// When your mouse leaves the CSH mode button, the body's background should go back to black and the lightbulb p tags text should go back to "💡"
+
+
+
+
+
+
+
+
+// Task 3: On and off feature
+// Set up the DOM manipulation flow such that when you click any of the lightbulb p tags, the p tag's background color toggles from black to white
+// You should use the classList property with the appropriate class from style.css
+
+
+
+
+
+
